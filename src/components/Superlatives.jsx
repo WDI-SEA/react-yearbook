7290f912de01e91525976fbc7f7dccfedf89cd7c
abelmark <mabel16113@gmail.com>
2016-03-30 00:33:55 -0700

    added random superlatives as well

--- src/components/Superlatives.jsx
+++ src/components/Superlatives.jsx
@@ -24,6 +24,26 @@ const Superlatives = React.createClass({
       </div>
     );
 
+    let awardsArray = [
+  		"Most Likely to Forget Breathing",
+  		"Most Likely To Pee Pants",
+  		"Most likely to Drink Bleach",
+  		"Most Likely to Marry Cousin",
+  		"Most Likely to Eat Their Boogers",
+  		"Most Likely to Poop Their Pants",
+  		"Most Likely to Sniff Their Fart",
+  		"Most Likely to Be A Virgin",
+  		"Most Likely to Vote for Trump",
+  		"Most Likely to Vote for Hillary",
+  		"Most Likely to Get Fat",
+  		"Most Likely to Watch American Idol",
+  		"Most Likely to Never Get Laid"
+  	];
+
+  	let theseAwards = []
+  	for(var i = 0; i < 3; i++){
+  		theseAwards.push(awardsArray.splice(Math.floor(Math.random() * awardsArray.length - 1), 1));
+  	};
 
     let people = this.state.people.map((person, idx) => {
       return <Person key={idx} idx={idx} person={person} />;
@@ -31,25 +51,28 @@ const Superlatives = React.createClass({
 
     let randomNumArr = [];
   	for(i = 0; i < 3; i++){
-  		randomNumArr.push(Math.floor(Math.random() * people.length));
+  		randomNum = Math.floor(Math.random() * people.length - 1);
+  		if(randomNumArr.indexOf(randomNum) == -1){
+  			randomNumArr.push(randomNum);
+  		}
     }
     return (
       <div className="text-center">
         <h1>Superlatives</h1>
         <div className="row">	
-        	<p>{people[randomNumArr[0]]}</p>
-      		<p>{people[randomNumArr[1]]}</p>
-      		<p>{people[randomNumArr[2]]}</p>
+        	{people[randomNumArr[0]]}
+      		{people[randomNumArr[1]]}
+      		{people[randomNumArr[2]]}
 				</div>
         <div className="row">
         	<div className="col-sm-4">
-	        	<h2>Most Likely to Forget Breathing</h2>
+	        	<h2>{theseAwards[0]}</h2>
         	</div>
         	<div className="col-sm-4">
-        		<h2>Most Likely To Pee Pants</h2>
+        		<h2>{theseAwards[1]}</h2>
         	</div>
         	<div className="col-sm-4">
-        		<h2>Most likely to Drink Bleach</h2>
+        		<h2>{theseAwards[2]}</h2>
         	</div>
         </div>
         <Link to={'/'}>Back</Link>
