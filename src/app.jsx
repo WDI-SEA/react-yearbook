123ef696e77a8687f83ec34d81489d44daf39b06
aheitzen <alexaheitzenrader@yahoo.com>
2016-03-29 22:00:41 -0700

    oh man

--- src/app.jsx
+++ src/app.jsx
@@ -3,10 +3,20 @@ const ReactDOM = require('react-dom');
 const YearBook = require('./components/YearBook');
 const PersonShow = require('./components/PersonShow');
 
+const Router = require('react-router').Router;
+const Route = require('react-router').Route;
+const browserHistory = require('react-router').browserHistory;
+
+
 const App = React.createClass({
   render: function() {
     return (
       <div>
+      <Router history={browserHistory}>
+        <Route path="/" component={YearBook} />
+        <Route path="/people/:id" component={PersonShow} />
+      </Router>
+      
         <h1>Yearbook Component</h1>
         <YearBook />
         <h1>PersonShow Component (Hardcoded to Person 0)</h1>
